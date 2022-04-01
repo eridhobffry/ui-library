8361bc74e819419cb4fec4b3e77e050ca2952a3c
Eridho Buffery Rollian <rollian@hanseaticsoft.com>
2022-04-01 14:44:43 +0200

    what needed

--- dist/cjs/types/components/button/button.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface ButtonProps {
-    label: string;
-    onButtonClick: () => void;
-}
-declare const Button: React.FC<ButtonProps>;
-export default Button;
--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface ButtonProps {
-    label: string;
-    onButtonClick: () => void;
-}
-declare const Button: React.FC<ButtonProps>;
-
-export { Button as RollButton };
